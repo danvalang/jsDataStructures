3dc9338987073fec413de121a13a9afb05b1fdb0
dvalle-nearsoft <dvalle@nearsoft.com>
2015-05-13 11:13:17 -0700

    Added tests

---
+++ test/stackSpec.js
@@ -0,0 +1,27 @@
+var expect = require("chai").expect;
+var should = require('chai').should();
+var Stack = require("../src/stack.js");
+
+describe("DataStructures", function() {
+  describe("#Stack", function() {
+    it("should build a stack on the constructor", function() {
+      var stack = new Stack(1, 2, 3, 4, 5);
+      stack.should.not.have.a.property('index');
+      stack.should.have.a.property('elements');
+    });
+    it("should be LIFO", function() {
+      var stack = new Stack(1, 2, 3, 4, 5);
+      expect(stack.pop()).to.be.equal(5);
+      expect(stack.pop()).to.be.equal(4);
+      stack.push("test");
+      expect(stack.pop()).to.be.equal("test");
+    });
+    describe("pop", function() {
+      it("should return null on empty stacks", function() {
+        var stack = new Stack(1);
+        expect(stack.pop()).to.be.equal(1);
+        expect(stack.pop()).to.be.equal(null);
+      });
+    });
+  });
+});
