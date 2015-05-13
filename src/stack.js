043747dffaf775fcf195f908a291c32ba106f7c7
dvalle-nearsoft <dvalle@nearsoft.com>
2015-05-13 11:13:34 -0700

    Simplified stack structure

--- src/stack.js
+++ src/stack.js
@@ -1,22 +1,29 @@
 module.exports = (function() {
-  'use strict';
 
-  index = null;
+  var index = null;
 
-  function Stack() {
-    this.elements = {};
-    DataStructure.call(this);
-  }
-  Stack.prototype.push = function(element) {
-    index = (!! index ? 0 : index + 1);
-    this.elements[index] = element;
+  return function() {
+    this.push = function(element) {
+      index++;
+      this.elements[index] = element;
+    };
+    this.pop = function() {
+      if (index > 0) {
+        var element = this.elements[index];
+        delete this.elements[index];
+        index = ((index - 1) < 0) ? null : index - 1;
+        return element;
+      } else {
+        return null;
+      }
+    };
+
+    this.elements = [];
+    index = 0;
+    if (arguments.length >= 1) {
+      for (var i = 0; i < arguments.length; i++) {
+        this.push(arguments[i]);
+      }
+    }
   };
-  Stack.prototype.pop = function() {
-    var element = this.elements[index];
-    delete this.elements[index];
-    index = (index - 1 < 0) ? null : index - 1;
-    return element;
-  };
-  return Stack;
 }());
-
