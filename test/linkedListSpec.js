09129b19c6409b2f7d701418bcd044f62413c2ee
dvalle-nearsoft <dvalle@nearsoft.com>
2015-05-18 16:58:15 -0700

    test refactor

--- test/linkedListSpec.js
+++ test/linkedListSpec.js
@@ -2,73 +2,68 @@ var LinkedList = require('../src/linkedList').LinkedList;
 var Node = require('../src/linkedList').Node;
 var expect = require("chai").expect;
 var should = require('chai').should();
-describe('DataStructures', function() {
-    describe('LinkedList', function() {
-        before(function() {
-            list = new LinkedList('hello');
-        });
-        it('should be able to insert a node at the beggining', function() {
-            list.insertBeggining('dan says');
-            expect(list.startNode.data).to.be.equal('dan says');
-            expect(list.startNode.next.data).to.be.equal('hello');
-        });
-        it('should be able to go to the beggining', function() {
-            list.insertBeggining('dan says3');
-            list.goStart();
-            expect(list.currentNode.data).to.be.equal('dan says3');
-        });
-        it('should be able to insert a node at the end', function() {
-            list.insertEnd('end node');
-            expect(list.endNode.data).to.be.equal('end node');
-        });
-        it('should be able to go to the end', function() {
-            list.insertAfter('in the middle');
-            list.insertEnd('End Point');
-            list.goEnd();
-            expect(list.endNode.data).to.be.equal('End Point');
-        });
-        it('should be able to insert a node after another', function() {
-            list.insertAfter("Este nodo");
-            expect(list.currentNode.data).to.be.equal(list.endNode.data);
-        });
-        it('should be able to insert a node before another', function() {
-            var expected = list.insertAfter("Este nodo");
-            list.insertBefore("antes del otro");
-            expect(list.currentNode.next).to.be.equal(expected);
-        });
-        it('should be able to delete a node ', function() {
-          list.insertBeggining("Hello");
-          list.goStart();
-          list.insertAfter(" pretty ");
-          list.insertAfter("World");
-          list.goPrevious();
-          list.deleteCurrent();
-          list.goStart();
-          expect(list.startNode.next.data).to.be.equal("World");
-        });
-        describe('#node', function() {
-            before(function() {
-                node = new Node("hello");
-            });
-            beforeEach(function() {
-                node = new Node("hello");
-            });
-            it('should be able to be created', function() {
-                node.should.have.property('data');
-                node.should.have.property('next');
-                node.should.have.property('previous');
-            });
-            it('should have a previous and next properties', function() {
-                node.should.have.property('next');
-                node.should.have.property('previous');
-            });
-            it('should be able to be linked to other nodes', function() {
-                var node2 = new Node('World');
-                node.linkNext(node2);
-                expect(node2.previous).to.be.equal(node);
-                node.linkPrevious(node2);
-                expect(node2.next).be.equal(node);
-            });
-        });
+describe('LinkedList', function() {
+  before(function() {
+    list = LinkedList('hello');
+  });
+  it('should be able to insert a node at the beggining', function() {
+    list.insertBeggining('dan says');
+    expect(list.startNode.data).to.be.equal('dan says');
+    expect(list.startNode.next.data).to.be.equal('hello');
+  });
+  it('should be able to go to the beggining', function() {
+    list.insertBeggining('dan says3');
+    list.goStart();
+    expect(list.currentNode.data).to.be.equal('dan says3');
+  });
+  it('should be able to insert a node at the end', function() {
+    list.insertEnd('end node');
+    expect(list.endNode.data).to.be.equal('end node');
+  });
+  it('should be able to go to the end', function() {
+    list.insertAfter('in the middle');
+    list.insertEnd('End Point');
+    list.goEnd();
+    expect(list.endNode.data).to.be.equal('End Point');
+  });
+  it('should be able to insert a node after another', function() {
+    list.insertAfter("Este nodo");
+    expect(list.currentNode.data).to.be.equal(list.endNode.data);
+  });
+  it('should be able to insert a node before another', function() {
+    var expected = list.insertAfter("Este nodo");
+    list.insertBefore("antes del otro");
+    expect(list.currentNode.next).to.be.equal(expected);
+  });
+  it('should be able to delete a node ', function() {
+    list.insertBeggining("Hello");
+    list.goStart();
+    list.insertAfter(" pretty ");
+    list.insertAfter("World");
+    list.goPrevious();
+    list.deleteCurrent();
+    list.goStart();
+    expect(list.startNode.next.data).to.be.equal("World");
+  });
+  describe('#node', function() {
+    before(function() {
+      node = Node("hello");
     });
+    it('should be able to be created', function() {
+      node.should.have.property('data');
+      node.should.have.property('next');
+      node.should.have.property('previous');
+    });
+    it('should have a previous and next properties', function() {
+      node.should.have.property('next');
+      node.should.have.property('previous');
+    });
+    it('should be able to be linked to other nodes', function() {
+      var node2 = Node('World');
+      node.linkNext(node2);
+      expect(node2.previous).to.be.equal(node);
+      node.linkPrevious(node2);
+      expect(node2.next).be.equal(node);
+    });
+  });
 });
--- test/stackSpec.js
+++ test/stackSpec.js
@@ -1,26 +1,24 @@
 var expect = require("chai").expect;
 var stack = require("../src/stack.js")();
 
-describe("DataStructures", function() {
-  describe("Stack", function() {
-    it('should have initial length of 0', function() {
-      expect(stack.length()).to.be.equal(0);
+describe("Stack", function() {
+  it('should have initial length of 0', function() {
+    expect(stack.length()).to.be.equal(0);
+  });
+  describe("should be LIFO", function() {
+    it('should push', function() {
+      stack.push(2);
+      stack.push(3);
+      expect(stack.length()).to.be.equal(2);
     });
-    describe("should be LIFO", function() {
-      it('should push', function () {
-        stack.push(2);
-        stack.push(3);
-        expect(stack.length()).to.be.equal(2);
-      });
-      it('should peek on top', function () {
-        expect(stack.peek()).to.be.equal(3);
-      });
-      it('should let you pop the last element', function () {
-        expect(stack.pop()).to.be.equal(3);
-      });
-      it('should have final length of 1', function () {
-        expect(stack.length()).to.be.equal(1);
-      });
+    it('should peek on top', function() {
+      expect(stack.peek()).to.be.equal(3);
+    });
+    it('should let you pop the last element', function() {
+      expect(stack.pop()).to.be.equal(3);
+    });
+    it('should have final length of 1', function() {
+      expect(stack.length()).to.be.equal(1);
     });
   });
 });
