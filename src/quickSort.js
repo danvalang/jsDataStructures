c95eafa8bc4dad26bac5e460a7e5853a0557d4b2
dvalle-nearsoft <dvalle@nearsoft.com>
2015-05-19 11:29:57 -0700

    QuickSort refactoring

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,25 +1,32 @@
 function QuickSort(data) {
-  var size = data.length - 1;
-  var leftArray = [];
-  var rightArray = [];
-  if (data.length < 2) {
+  if (data.length < 1) {
     return data;
   } else {
-    var pivot = data[size];
-    for (var index = 0; index < size; index++) {
-      value = data[index];
-      if (value < pivot)
-        leftArray.push(value);
-      if (value > pivot)
-        rightArray.push(value);
+    return conquer(data);
+  }
+
+  function conquer(data) {
+    var size = data.length - 1;
+    return divide(data);
+
+    function divide(data) {
+      var leftArray = [];
+      var rightArray = [];
+      var pivot = data[size];
+      for (var index = 0; index < size; index++) {
+        value = data[index];
+        if (value < pivot)
+          leftArray.push(value);
+        if (value > pivot)
+          rightArray.push(value);
+      }
+      if (leftArray.length > 1)
+        leftArray = QuickSort(leftArray);
+      if (rightArray.length > 1)
+        rightArray = QuickSort(rightArray);
+      return leftArray.concat([pivot], rightArray);
+
     }
-    if (leftArray.length > 2)
-      leftArray = QuickSort(leftArray);
-    if (rightArray.length > 2)
-      rightArray = QuickSort(rightArray);
-    data = leftArray.concat([pivot], rightArray);
-    return data;
   }
 }
-
 module.exports = QuickSort;
