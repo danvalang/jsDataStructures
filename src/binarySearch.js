a16ae847e119a53a83181b0bc0530821f3df3c4f
dvalle-nearsoft <dvalle@nearsoft.com>
2015-05-19 14:55:45 -0700

    Added Binary Search

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -1,4 +1,36 @@
-var LinkedList = require('./linkedList');
-function binarySearch(argument) {
-  // body...
+function BinarySearch(data, target, callBack) {
+  if (data instanceof Array && callBack instanceof Function) {
+    return callBack(searchbyIndex(0, data.length - 1));
+  } else {
+    throw new Error("Invalid Arguments");
+  }
+
+
+  function searchbyIndex(start, end) {
+    try {
+      if (start >= end || (end <= 1) && start === 0) {
+        if (data[end] == target) {
+          return target;
+        }
+        throw new Error("target not found");
+      } else {
+        var middle = Math.floor(end - start / 2);
+        var value = data[middle];
+        if (target > value) {
+          return searchbyIndex(middle - 1, end);
+        }
+        if (target < value) {
+          return searchbyIndex(start, middle - 1);
+        }
+        return middle;
+      }
+
+    } catch (e) {
+      console.log(start, end);
+      console.error(e);
+      throw (e);
+    }
+  }
 }
+
+module.exports = BinarySearch;
--- test/binarySearchSpec.js
+++ test/binarySearchSpec.js
@@ -1,6 +1,25 @@
-var expect = require("chai").expect;
 var BinarySearch = require('../src/binarySearch');
-describe('Binary Search', function () {
-  it('should search for a number');
-  it('should search over a string');
+var QuickSort = require('../src/quickSort');
+var expect = require('chai').expect;
+describe('Binary Search', function() {
+  var fixtureInt, fixtureString;
+  before(function() {
+    fixtureInt = QuickSort([9, 7, 5, 11, 12, 2, 14, 3, 10, 6]);
+    fixtureString = QuickSort(['a', 'v', 'q', 'f', 'dan', 'mira', 'soy', 'yo']);
+  });
+  it('should throw and error when invalid arguments are given', function() {
+    expect(BinarySearch).to.throw(Error);
+  });
+  it('should search for a number', function(done) {
+    BinarySearch(fixtureInt, 11, function(result) {
+      expect(result).to.be.ok;
+      done();
+    });
+  });
+  it('should search over a string', function(done) {
+    BinarySearch(fixtureString, 'dan', function(result) {
+      expect(result).to.be.ok;
+      done();
+    });
+  });
 });
