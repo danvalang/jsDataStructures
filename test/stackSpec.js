f3a30e3e661f6275173455b28c89f4dd1a703d2b
dvalle-nearsoft <dvalle@nearsoft.com>
2015-06-02 17:00:28 -0700

    Made Tests atomic

--- test/stackSpec.js
+++ test/stackSpec.js
@@ -1,7 +1,11 @@
 var expect = require("chai").expect;
-var stack = require("../src/stack.js")();
+var Stack = require("../src/stack.js");
 
 describe("Stack", function() {
+	var stack;
+	beforeEach(function () {
+		stack = Stack();
+	});
 	it('should have initial length of 0', function() {
 		expect(stack.length()).to.be.equal(0);
 	});
@@ -12,12 +16,15 @@ describe("Stack", function() {
 			expect(stack.length()).to.be.equal(2);
 		});
 		it('should peek on top', function() {
+			stack.push(3);
 			expect(stack.peek()).to.be.equal(3);
 		});
 		it('should let you pop the last element', function() {
+			stack.push(3);
 			expect(stack.pop()).to.be.equal(3);
 		});
 		it('should have final length of 1', function() {
+			stack.push(3);
 			expect(stack.length()).to.be.equal(1);
 		});
 	});
